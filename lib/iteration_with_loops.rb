7cb8fe6135f8c4b91cb3dd2dfda73e761e0cc402
interpreted-validator-6256 <danielle.leppertsimenauer@gmail.com>
2020-11-23 21:24:19 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,22 @@
 def join_nested_strings(src)
-  # src will be an Array of Arrays of Strings and Integers
-  # Combine all Strings present in the AoA into a single value and return it
+  total = []
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
+      if src[row_index][element_index].instance_of?(String)
+        total = total.join(src[row_index][element_index])
+      element_index += 1
+    end
+    row_index += 1 
+  end
+print total
 end
+
+mixed_data = [
+  ["The", 4, "quick"],
+  [-1, "brown", "fox", 30],
+  ["studied", 101, 233, "Ruby"]
+]
+
+join_nested_strings(guessing_game_grid)
